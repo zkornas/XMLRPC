c1fe302a1dd0e54d60ce4ea8e2d0ca96d0e773af
zkornas <zkornas@uw.edu>
2023-05-10 20:51:46 -0700

    Added exceptions

--- xmlclient.py
+++ xmlclient.py
@@ -5,6 +5,13 @@ import requests
 headers = {'Content-type': 'application/xml'}
 url = 'http://localhost:8080'
 
+def getText(nodelist):
+    rc = []
+    for node in nodelist:
+        if node.nodeType == node.TEXT_NODE:
+            rc.append(node.data)
+    return ''.join(rc)
+
 def helper(method, *args):
     xml_params = ''.join([f'<i4>{arg}</i4>' for arg in args])
     xml_data = f'''
@@ -21,23 +28,44 @@ def helper(method, *args):
     response = requests.post(url, headers=headers, data=xml_data.strip())
     response_str = response.content.decode('utf-8').strip()
     xml_response = xml.dom.minidom.parseString(response_str)
-    ans = xml_response.getElementsByTagName("i4")[0].childNodes[0].nodeValue
-    return int(ans)
+    fault_code_node = xml_response.getElementsByTagName("struct")
+    if fault_code_node:
+        fault_code = xml_response.getElementsByTagName("int")[0].childNodes[0].nodeValue
+        fault_string = xml_response.getElementsByTagName("string")[0].childNodes[0].nodeValue
+        raise Exception(f"Fault Code: {fault_code}, Fault String: {fault_string}")
+    else:
+        ans = xml_response.getElementsByTagName("i4")[0].childNodes[0].nodeValue
+        return int(ans)
 
 def add(*args):
-    return(helper("add", *args))
+    try:
+        return helper("add", *args)
+    except Exception as e:
+        print(f"Exception occurred: {e}")
 
 def subtract(*args):
-    return(helper("subtract", *args))
+    try:
+        return helper("subtract", *args)
+    except Exception as e:
+        print(f"Exception occurred: {e}")
 
 def multiply(*args):
-    return(helper("multiply", *args))
+    try:
+        return helper("multiply", *args)
+    except Exception as e:
+        print(f"Exception occurred: {e}")
 
 def divide(*args):
-    return(helper("divide", *args))
+    try:
+        return helper("divide", *args)
+    except Exception as e:
+        print(f"Exception occurred: {e}")
 
 def modulo(*args):
-    return(helper("modulo", *args))
+    try:
+        return helper("modulo", *args)
+    except Exception as e:
+        print(f"Exception occurred: {e}")
 
 
 print(add() == 0)
@@ -48,3 +76,11 @@ print(multiply(3, 4) == 12)
 print(multiply(1, 2, 3, 4, 5) == 120)
 print(divide(10, 5) == 2)
 print(modulo(10, 5) == 0)
+print(f'\n')
+
+#exceptions
+print(multiply(1000000, 100000))
+print(subtract('twelve', 'six'))
+print(divide(10, 0))
+print(add(9999999999999, 99999999999))
+
--- xmlserver.py
+++ xmlserver.py
@@ -11,9 +11,6 @@ def getText(nodelist):
             rc.append(node.data)
     return ''.join(rc)
 
-def add(x):
-    return(x)
-
 class XMLServer(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(405)
@@ -39,58 +36,158 @@ class XMLServer(BaseHTTPRequestHandler):
         for i in params_node:
             params.append(getText(i.childNodes))
 
-        # math operations
-        if method_name == "add":
-            params_int = [int(i) for i in params]
-            result = sum(params_int)
-        elif method_name == "subtract":
-            params_int = [int(i) for i in params]
-            result = params_int[0] - sum(params_int[1:])
-        elif method_name == "multiply":
-            params_int = [int(i) for i in params]
-            result = 1
-            for p in params_int:
-                result *= p
-        elif method_name == "divide":
-            params_int = [int(i) for i in params]
-            result = params_int[0]
-            for d in params_int[1:]:
-                result //= d
-        elif method_name == "modulo":
-            params_int = [int(i) for i in params]
-            result = params_int[0] % params_int[1]
-        else:
-            # Handle unknown method
+        # Handle math operations and exceptions
+        try:
+            if method_name == "add":
+                params_int = [int(i) for i in params]
+                # Check for overflow before performing the addition
+                if any(p > (10**9 - sum(params_int[:idx])) for idx, p in enumerate(params_int, start=1)):
+                    raise OverflowError("Overflow fault")
+                result = sum(params_int)
+            elif method_name == "subtract":
+                if len(params) != 2 or not params[0].isdigit() or not params[1].isdigit():
+                    raise ValueError("Illegal argument fault")
+                params_int = [int(i) for i in params]
+                result = params_int[0] - sum(params_int[1:])
+            elif method_name == "multiply":
+                params_int = [int(i) for i in params]
+                result = 1
+                for p in params_int:
+                    result *= p
+                # Check for overflow
+                if result > 10**9:
+                    raise OverflowError("Overflow fault")
+            elif method_name == "divide":
+                if len(params) != 2 or not params[0].isdigit() or not params[1].isdigit():
+                    raise ValueError("Illegal argument fault")
+                params_int = [int(i) for i in params]
+                dividend, divisor = params_int[0], params_int[1]
+                if divisor == 0:
+                    raise ZeroDivisionError("Divide-by-zero fault")
+                result = dividend // divisor
+            elif method_name == "modulo":
+                if len(params) != 2 or not params[0].isdigit() or not params[1].isdigit():
+                    raise ValueError("Illegal argument fault")
+                params_int = [int(i) for i in params]
+                dividend, divisor = params_int[0], params_int[1]
+                if divisor == 0:
+                    raise ZeroDivisionError("Divide-by-zero fault")
+                result = dividend % divisor
+            else:
+                raise ValueError("Unknown method")
+
+            # Construct response
+            response_str = """
+                <?xml version="1.0"?>
+                <methodResponse>
+                    <params>
+                        <param>
+                            <value><i4>{}</i4></value>
+                        </param>
+                    </params>
+                </methodResponse>
+            """.format(result)
+
+            print(response_str)
+            self.send_response(200)
+            self.send_header("Content-type", "application/xml")
+            self.end_headers()
+            self.wfile.write(response_str.encode('utf-8'))
+
+        except ValueError as e:
+                    # Handle illegal argument fault
+                    response_str = """
+                        <?xml version="1.0"?>
+                        <methodResponse>
+                            <fault>
+                                <value>
+                                    <struct>
+                                        <member>
+                                            <name>faultCode</name>
+                                            <value><int>400</int></value>
+                                        </member>
+                                        <member>
+                                            <name>faultString</name>
+                                            <value><string>{}</string></value>
+                                        </member>
+                                    </struct>
+                                </value>
+                            </fault>
+                        </methodResponse>
+                    """.format(str(e))
+
+                    print(response_str)
+                    self.send_response(400)
+                    self.send_header("Content-type", "application/xml")
+                    self.end_headers()
+                    self.wfile.write(response_str.encode('utf-8'))
+
+        except ZeroDivisionError as e:
+            # Handle divide-by-zero fault
+            response_str = """
+                <?xml version="1.0"?>
+                <methodResponse>
+                    <fault>
+                        <value>
+                            <struct>
+                                <member>
+                                    <name>faultCode</name>
+                                    <value><int>400</int></value>
+                                </member>
+                                <member>
+                                    <name>faultString</name>
+                                    <value><string>{}</string></value>
+                                </member>
+                            </struct>
+                        </value>
+                    </fault>
+                </methodResponse>
+            """.format(str(e))
+
+            print(response_str)
+            self.send_response(400)
+            self.send_header("Content-type", "application/xml")
+            self.end_headers()
+            self.wfile.write(response_str.encode('utf-8'))
+
+        except OverflowError as e:
+            # Handle overflow fault
+            response_str = """
+                <?xml version="1.0"?>
+                <methodResponse>
+                    <fault>
+                        <value>
+                            <struct>
+                                <member>
+                                    <name>faultCode</name>
+                                    <value><int>400</int></value>
+                                </member>
+                                <member>
+                                    <name>faultString</name>
+                                    <value><string>{}</string></value>
+                                </member>
+                            </struct>
+                        </value>
+                    </fault>
+                </methodResponse>
+            """.format(str(e))
+
+            print(response_str)
+            self.send_response(400)
+            self.send_header("Content-type", "application/xml")
+            self.end_headers()
+            self.wfile.write(response_str.encode('utf-8'))
+
+        except Exception as e:
+            # Handle unknown method fault
             response_str = xml_data.toprettyxml()
             print(response_str)
             self.send_response(400)
             self.send_header("Content-type", "application/xml")
             self.end_headers()
             self.wfile.write(response_str.encode('utf-8'))
-            return
 
-        # Construct response
-        response_str = """
-            <?xml version="1.0"?>
-            <methodResponse>
-                <params>
-                    <param>
-                        <value><i4>{}</i4></value>
-                    </param>
-                </params>
-            </methodResponse>
-        """.format(result)
-
-        print(response_str)
-        self.send_response(200)
-        self.send_header("Content-type", "application/xml")
-        self.end_headers()
-        self.wfile.write(response_str.encode('utf-8'))
-
-
-
-
-
+# Create and start the server
 server = HTTPServer((host, port), XMLServer)
 print("Starting server...")
 server.serve_forever()
